110b10a3ae86779ce6bb6fe24b38e48a0e05d6c3
Abdullah's Macbook Air <abdullah@192.168.1.2>
2021-11-30 18:57:05 +0500

    first commit

---
+++ js/custom.js
@@ -0,0 +1,46 @@
+var wow = new WOW(
+  {
+    boxClass:     'wow',      // animated element css class (default is wow)
+    animateClass: 'animated', // animation css class (default is animated)
+    offset:       0,          // distance to the element when triggering the animation (default is 0)
+    mobile:       true,       // trigger animations on mobile devices (default is true)
+    live:         true,       // act on asynchronously loaded content (default is true)
+    callback:     function(box) {
+      // the callback is fired every time an animation is started
+      // the argument that is passed in is the DOM node being animated
+    },
+    scrollContainer: null // optional scroll container selector, otherwise use window
+  }
+);
+wow.init();
+
+
+
+
+const data=[
+{
+  imgURL:'img/boy.png'
+
+},
+{
+   imgURL:'img/boy.png'
+}
+]
+
+let counter=0
+
+let btnNext=document.querySelector('#btn-next')
+
+let imgNext=document.querySelector('#img-next')
+
+
+function getData(){
+  let datas=data[counter];
+  imgNext.src=datas.imgURL
+}
+
+btnNext.addEventListner('click',()=>{
+  counter ++;
+  console.log('clicks')
+  getData();
+})
