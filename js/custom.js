d9fdc943367c877a1dd87054b6abd63ddac474f7
abdullahafzal-greelogix <abdullahafzal@greelogix.com>
2021-12-07 20:10:07 +0500

    changes

--- js/custom.js
+++ js/custom.js
@@ -1,6 +1,6 @@
 
 var wow = new WOW(
-  {
+{
     boxClass:     'wow',      // animated element css class (default is wow)
     animateClass: 'animated', // animation css class (default is animated)
     offset:       0,          // distance to the element when triggering the animation (default is 0)
@@ -12,29 +12,29 @@ var wow = new WOW(
     },
     scrollContainer: null // optional scroll container selector, otherwise use window
   }
-);
+  );
 wow.init();
 
 
 
 
 function myFunction() {
-      var x = document.getElementById("Demo");
-      if (x.className.indexOf("w3-show") == -1) { 
-        x.className += " w3-show";
-      } else {
-        x.className = x.className.replace(" w3-show", "");
-      }
-    }
+  var x = document.getElementById("Demo");
+  if (x.className.indexOf("w3-show") == -1) { 
+    x.className += " w3-show";
+  } else {
+    x.className = x.className.replace(" w3-show", "");
+  }
+}
 
 
 
 
-    var acc = document.getElementsByClassName("accordion");
-    var i;
+var acc = document.getElementsByClassName("accordion");
+var i;
 
-    for (i = 0; i < acc.length; i++) {
-      acc[i].addEventListener("click", function() {
+for (i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function() {
         /* Toggle between adding and removing the "active" class,
         to highlight the button that controls the panel */
         this.classList.toggle("active");
@@ -47,39 +47,39 @@ function myFunction() {
           panel.style.display = "block";
         }
       });
-    }
+}
 
 
 
 
 
-    function up(max) {
-    document.getElementById("myNumber-two").value = parseInt(document.getElementById("myNumber-two").value) + 1;
-    if (document.getElementById("myNumber-two").value >= parseInt(max)) {
-        document.getElementById("myNumber-two").value = max;
-    }
+function up(max) {
+  document.getElementById("myNumber-two").value = parseInt(document.getElementById("myNumber-two").value) + 1;
+  if (document.getElementById("myNumber-two").value >= parseInt(max)) {
+    document.getElementById("myNumber-two").value = max;
+  }
 }
 function down(min) {
-    document.getElementById("myNumber-two").value = parseInt(document.getElementById("myNumber-two").value) - 1;
-    if (document.getElementById("myNumber-two").value <= parseInt(min)) {
-        document.getElementById("myNumber-two").value = min;
-    }
+  document.getElementById("myNumber-two").value = parseInt(document.getElementById("myNumber-two").value) - 1;
+  if (document.getElementById("myNumber-two").value <= parseInt(min)) {
+    document.getElementById("myNumber-two").value = min;
+  }
 }
 
 
 
 
-    function up(max) {
-    document.getElementById("myNumber").value = parseInt(document.getElementById("myNumber").value) + 1;
-    if (document.getElementById("myNumber").value >= parseInt(max)) {
-        document.getElementById("myNumber").value = max;
-    }
+function up(max) {
+  document.getElementById("myNumber").value = parseInt(document.getElementById("myNumber").value) + 1;
+  if (document.getElementById("myNumber").value >= parseInt(max)) {
+    document.getElementById("myNumber").value = max;
+  }
 }
 function down(min) {
-    document.getElementById("myNumber").value = parseInt(document.getElementById("myNumber").value) - 1;
-    if (document.getElementById("myNumber").value <= parseInt(min)) {
-        document.getElementById("myNumber").value = min;
-    }
+  document.getElementById("myNumber").value = parseInt(document.getElementById("myNumber").value) - 1;
+  if (document.getElementById("myNumber").value <= parseInt(min)) {
+    document.getElementById("myNumber").value = min;
+  }
 }
 
 
@@ -88,14 +88,14 @@ function down(min) {
 
 
 $(window).scroll(function() {
-    var distanceFromTop = $(this).scrollTop();
-    if (distanceFromTop >= $('#sticky').height()) {
-        $('#sticky').addClass('fixed');
-        $('#sticky').slideDown(500);
-    } else {
-        $('#sticky').hide();
-        $('#sticky').removeClass('fixed');
-    }
+  var distanceFromTop = $(this).scrollTop();
+  if (distanceFromTop >= $('#sticky').height()) {
+    $('#sticky').addClass('fixed');
+    $('#sticky').slideDown(500);
+  } else {
+    $('#sticky').hide();
+    $('#sticky').removeClass('fixed');
+  }
 });
 
 
@@ -116,3 +116,22 @@ $(window).load(function() {
   $(".loader").delay(2000).fadeOut("slow");
   $("#overlayer").delay(2000).fadeOut("slow");
 })
+
+
+
+
+
+var btn = $('#button');
+
+$(window).scroll(function() {
+  if ($(window).scrollTop() > 300) {
+    btn.addClass('show');
+  } else {
+    btn.removeClass('show');
+  }
+});
+
+btn.on('click', function(e) {
+  e.preventDefault();
+  $('html, body').animate({scrollTop:0}, '300');
+});
